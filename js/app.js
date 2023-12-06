d208dd7c9e1f82bc785cec03887bd56243cf0598
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-12-06 12:32:39 -0500

    se creo la animacion de la nieve

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
 
+    // Temporizador
+    const countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
     const x = setInterval(function () {
         const now = new Date().getTime();
         const distance = countDownDate - now;
@@ -20,4 +21,20 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("timer").innerHTML = "¡Feliz Navidad!";
         }
     }, 1000);
+
+    //carga de nieve
+    const snowflakesContainer = document.querySelector(".snowflakes");
+    const cantidadNieve=100
+
+    for (let i = 0; i < cantidadNieve; i++) {
+        const snowflake = document.createElement("div");
+        snowflake.className = "snowflake";
+        snowflake.style.left = `${Math.random() * 100}vw`;
+        snowflake.style.animationDuration = `${Math.random() * 2 + 1}s`;
+        snowflake.style.animationDelay = `${Math.random()}s`;
+        snowflakesContainer.appendChild(snowflake);
+    }
 });
+
+
+
